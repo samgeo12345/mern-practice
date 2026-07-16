e998ac140b99d80d993f2c6fce454b8b73932626
Samuel George <samgeoj12d@gmail.com>
2026-07-16 22:48:56 +0530

    pushed

--- backend/server.js
+++ backend/server.js
@@ -25,30 +25,31 @@ app.get('/api/welcome', (req, res)=>{
     res.json({message: 'hello world!'})
 })
 
-app.post('/api/register', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
+//registration user
+app.post('/api/register', async(req, res) => {
+    try{
+        const { email, password } = req.body
+        const existingUser = await User.findOne({email})
+        if(existingUser){
+            return res.status(400).json({message: 'User already exists'})
         }
 
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
 
         const newUser = new User({
             email,
             password: hashedPassword
-        });
-        await newUser.save();
+        })
+        await newUser.save()
 
-        res.status(201).json({ message: 'User created successfully!' });
-
-    } catch (error) {
-        res.status(500).json({ message: 'Server error', error: error.message });
+        res.status(201).json({message: 'User created successfully!'})
     }
-});
+    catch(error)
+    {
+        res.status(500).json({message: 'server error', error: error.message})
+    }
+})
 
 app.post('/api/login', async (req, res) => {
     try {
